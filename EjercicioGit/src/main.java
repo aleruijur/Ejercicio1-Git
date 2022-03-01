1ed901fc507e7470596e856ae7d7da8abfeb9734
aleruijur <aleruijur@alum.us.es>
2022-03-01 13:50:53 +0100

    main con suma

---
+++ EjercicioGit/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int res1 = Suma.suma(2, 3);
+		System.out.println(res1);
+		int res2 = Suma.sumaAcum(3);
+		System.out.println(res2);
+		int res3 = Suma.sumaAcum(2);
+		System.out.println(res3);
+	}
+
+}
