6bcbbd9ab6fbbad3abea412aff64ecb8318bd16a
aleruijur <aleruijur@alum.us.es>
2022-03-02 13:11:27 +0100

    Añadido mensaje en main

--- EjercicioGit/src/main.java
+++ EjercicioGit/src/main.java
@@ -2,6 +2,8 @@
 public class main {
 
 	public static void main(String[] args) {
+		System.out.println("Pruebas:");
+		
 		int res1 = Suma.suma(2, 3);
 		System.out.println(res1);
 		int res2 = Suma.sumaAcum(3);
